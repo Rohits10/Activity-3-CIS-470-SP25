1958240287258944380d6f4208e6e8aac5424dd1
Rohit Shirke <115170718+Rohits10@users.noreply.github.com>
2025-02-26 03:47:16 +0000

    updated classifyTriangle test

--- classifyTriangle.test.js
+++ classifyTriangle.test.js
@@ -13,8 +13,93 @@ describe('classifyTriangle: Required input conditions -  See readme for more det
     });
   
     test('should return "Not a Triangle" for invalid triangle sides', () => {
+      expect(classifyTriangle(1, 1, 3)).toBe('Not a Triangle');
+      // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(0, 11, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(0, 12, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should classify an Equilateral triangle', () => {
+      expect(classifyTriangle(10, 11, 10)).toBe('Equilateral');
+      // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+  
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(0, 10, 11)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+  
+    test('should return "Not a Triangle" for invalid triangle sides', () => {
       expect(classifyTriangle(1, 2, 3)).toBe('Not a Triangle');
       // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(0, 10, 12)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(0, 10, 13)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should classify an Equilateral triangle', () => {
+      expect(classifyTriangle(12, 10, 10)).toBe('Equilateral');
+      // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+  
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(0, 10, 14)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+  
+    test('should return "Not a Triangle" for invalid triangle sides', () => {
+      expect(classifyTriangle(1, 1, 3)).toBe('Not a Triangle');
+      // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(10, 13, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(0, 12, 9)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should classify an Equilateral triangle', () => {
+      expect(classifyTriangle(11, 7, 8)).toBe('Equilateral');
+      // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+  
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(6, 10, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+  
+    test('should return "Not a Triangle" for invalid triangle sides', () => {
+      expect(classifyTriangle(3, 2, 3)).toBe('Not a Triangle');
+      // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(0, 5, 13)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+    });
+
+    test('should return error for invalid inputs', () => {
+      expect(classifyTriangle(0, 10, 2)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
     });
   });
   
